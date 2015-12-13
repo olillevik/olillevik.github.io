4447580adf536db6083f2ab7b8fd5bf32c499014
Ørjan Markhus Lillevik <olillevik@gmail.com>
2015-12-13 23:04:17 +0100

    Add tic-tac-toe

---
+++ README.md
@@ -0,0 +1,22 @@
+# Introduksjon til JavaScript
+_ved Ørjan Markhus Lillevik_
+
+## Hvorfor JavaScript i kodeklubben?
+Hvorfor er JavaScript verdens viktigste programmeringsspråk...
+
+## Hva skal vi lage
+Tripp-trapp-tresko
+
+## Programmering i JavaScript
+
+### Funksjoner
+
+### Variable
+
+### Objekter
+
+## HTML/CSS/JavaScript
+Dett#e er ikke et HTML-kurs men litt trenger vi for å lage brettet.
+
+## Verktøy
+Intro til Atom. Webside på Github pages?
---
+++ script.js
@@ -0,0 +1,47 @@
+(() => {
+
+  var player = "O";
+  var moves = [];
+  var winningCombos = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+    ["1", "5", "8"],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["3", "6", "9"]
+  ];
+
+  play = (square) => {
+    if (square.innerHTML !== "O" && square.innerHTML !== "X") {
+      mark(square);
+      saveMove(square);
+      if (isWinner()) {
+        alert("Wohoo! " + player + " vant!");
+        // more fun...
+        // count score
+        // reset game
+      };
+      // if (isDraw()){}
+      changePlayer();
+    }
+  }
+
+  mark = m => m.innerHTML = player;
+
+  changePlayer = () => {
+    if (player === "O") {
+      player = "X";
+    } else {
+      player = "O";
+    }
+  }
+
+  saveMove = (e) => moves[e.id] = player;
+
+  isWinner = () =>
+    return (winningCombos.map(checkCombo).filter(result => result).length > 0);
+
+  checkCombo = combo => (combo.filter(square => moves[square] === player).length === 3);
+
+})();
