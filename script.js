5a5eabb5afc44f95df38d4cf6c1d2ffd95d00f8a
Ørjan Markhus Lillevik <olillevik@gmail.com>
2015-12-14 22:43:52 +0100

    Reformat.

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 (() => {
     'use strict';
 
-    var player = "O",
-        moves = [],
-        winningCombos = [
-    ["1", "2", "3"],
-    ["4", "5", "6"],
-    ["7", "8", "9"],
-    ["1", "5", "9"],
-    ["3", "5", "7"],
-    ["1", "4", "7"],
-    ["2", "5", "8"],
-    ["3", "6", "9"]
-  ];
+    var player = "O";
+    var moves = [];
+    var winningCombos = [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"],
+        ["1", "5", "9"],
+        ["3", "5", "7"],
+        ["1", "4", "7"],
+        ["2", "5", "8"],
+        ["3", "6", "9"]];
 
     window.play = (sq) => {
         if (sq.innerHTML !== "O" && sq.innerHTML !== "X") {
@@ -31,7 +30,7 @@
 
     var drawMark = m => m.innerHTML = player;
 
-    var saveMove = (m) => moves[m.id] = player;
+    var saveMove = m => moves[m.id] = player;
 
     var changePlayer = () => {
         if (player === "O") {
