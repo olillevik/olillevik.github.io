1fad07b65232db312896344a258cce2913ec7fd6
Ørjan Markhus Lillevik <olillevik@gmail.com>
2016-02-05 21:06:32 +0100

    Separate model/update/view

--- script.js
+++ script.js
@@ -5,69 +5,78 @@
     var el = id => document.getElementById(id + "");
 
     // modell
-    var spiller = "0";
-    var poengX = "0";
-    var poeng0 = "0"
-    var trekk = [];
+        var spiller = "0";
+        var poengX = "0";
+        var poeng0 = "0";
+        var trekk = ["", "", "", "", "", "", "", "", ""];
+        var vinnerkombinasjon = [];
     
-    const brett = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    // konstanter
+    const brett = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
     const vinnerkombinasjoner = [
-        ["1", "2", "3"],
-        ["4", "5", "6"],
-        ["7", "8", "9"],
-        ["1", "5", "9"],
-        ["3", "5", "7"],
+        ["0", "1", "2"],
+        ["3", "4", "5"],
+        ["6", "7", "8"],
+        ["0", "4", "8"],
+        ["2", "4", "6"],
+        ["0", "3", "6"],
         ["1", "4", "7"],
-        ["2", "5", "8"],
-        ["3", "6", "9"]];
+        ["2", "5", "8"]];
 
-    // oppdater
     var gjoerTrekk = (felt) => {
         if (ledig(felt)) {
-            tegnMerke(felt);
+            // Oppdater modell
             lagreTrekk(felt);
-            fargVinner();
+            finnVinnerkombo();
             giPoeng();
             byttSpiller();
-        }
+            
+            // Oppdater webside
+            tegnMerke(felt);
+            fargVinnerkombo();
+            oppdaterPoeng();
+        }           
     };
 
-    var ledig = felt => trekk.filter(fylt => fylt === felt); 
-
-    var tegnMerke = felt => el(felt).innerHTML = spiller;
-
+    var ledig = felt => trekk[felt] === ""; 
+    
     var lagreTrekk = felt => trekk[felt] = spiller;
-
-    var fargVinner = () => vinnerkombinasjoner
-        .filter(eietAvSpiller)
-        .map(fargKombinasjon);
-
-    var giPoeng = () => {
-        if (vinnerkombinasjoner.filter(eietAvSpiller).length > 0) {
-            if (spiller === "X") {
-                el("spiller" + spiller).innerHTML = (++poengX);
-            } else if (spiller === "0") {
-                el("spiller" + spiller).innerHTML = (++poeng0);
-            }
-        }
-    };
-
+    
+    var finnVinnerkombo = () => vinnerkombinasjoner.filter(eietAvSpiller).map(kombo => vinnerkombinasjon.push(kombo)); 
     var eietAvSpiller = kombo => kombo
         .filter(felt => trekk[felt] === spiller)
         .length === 3;
-
-    var fargKombinasjon = kombo => kombo.map(felt => el(felt).style.backgroundColor = "green");
-
+    
+    var giPoeng = () => {
+        if (vinnerkombinasjon.length > 0) {
+            if (spiller === "X") {
+                ++poengX;
+            } else if (spiller === "0") {
+                ++poeng0;
+            }
+        }
+    };
+    
     var byttSpiller = () => (spiller === "0") ? spiller = "X" : spiller = "0";
-     
+
+    var tegnMerke = felt => el(felt).innerHTML = spiller; 
+
+    var fargVinnerkombo = () => vinnerkombinasjon.map(kombo => fargFelt(kombo));
+    var fargFelt = kombo => kombo.map(felt => el(felt).style.backgroundColor = "green");
+
+    var oppdaterPoeng = () => {
+        el("spiller" + spiller).innerHTML = (poengX);
+        el("spiller" + spiller).innerHTML = (++poeng0);
+    };
+    
     var nyttSpill = () => {
         brett.map(id => el(id).innerHTML = "&nbsp");
         brett.map(id => el(id).style.backgroundColor = "lightgray");
-        trekk = [];
+        trekk = ["", "", "", "", "", "", "", "", ""];
     }
     
     // hendelser
-    brett.map(id => el(id).addEventListener("click", () => gjoerTrekk(id)));    
+    brett.map(id => el(id).addEventListener("click", () => gjoerTrekk(id)));   
     el("nyttSpill").addEventListener("click", () => nyttSpill());
 
 })();
